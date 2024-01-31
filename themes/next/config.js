0786731b6e95f58cb027f82306b2349437612d95
CC.Chang <ksharry1025@gmail.com>
2024-01-31 11:01:54 +0800

    Update config.js

--- themes/next/config.js
+++ themes/next/config.js
@@ -1,7 +1,7 @@
 const CONFIG = {
 
-  NEXT_HOME_BANNER: true, // 首页是否显示大图及标语 [true,false]
-  NEXT_HOME_BANNER_STRINGS: ['Hi，我是一个程序员', 'Hi，我是一个打工人', 'Hi，我是一个干饭人', '欢迎来到我的博客🎉'], // 首页大图标语文字
+  NEXT_HOME_BANNER: true, // 首頁顯示大標
+  NEXT_HOME_BANNER_STRINGS: ['Hi，對顧問與新科技有興趣~🎉'], // 首頁大標文字
 
   NEXT_NAV_TYPE: 'normal', // ['fixed','autoCollapse','normal'] 分别是固定屏幕顶部并始终显示、固定屏幕顶部且滚动时隐藏，不固定屏幕顶部
 
@@ -12,22 +12,22 @@ const CONFIG = {
   NEXT_POST_HEADER_IMAGE_VISIBLE: false, // 文章详情页是否显示封面图
 
   // 右侧组件
-  NEXT_RIGHT_BAR: true, // 是否显示右侧栏
+  NEXT_RIGHT_BAR: false, // 是否显示右侧栏
   NEXT_RIGHT_LATEST_POSTS: true, // 右侧栏最新文章
   NEXT_RIGHT_CATEGORY_LIST: true, // 右侧边栏文章分类列表
   NEXT_RIGHT_TAG_LIST: true, // 右侧边栏标签分类列表
   NEXT_RIGHT_AD: false, // 右侧广告
 
   // 菜单
-  NEXT_MENU_HOME: true, // 显示首页
-  NEXT_MENU_CATEGORY: true, // 显示分类
-  NEXT_MENU_TAG: true, // 显示标签
-  NEXT_MENU_ARCHIVE: true, // 显示归档
-  NEXT_MENU_SEARCH: true, // 显示搜索
+  NEXT_MENU_HOME: true, // 首页
+  NEXT_MENU_CATEGORY: true, // 分類
+  NEXT_MENU_TAG: true, // 標籤
+  NEXT_MENU_ARCHIVE: true, // 歸檔
+  NEXT_MENU_SEARCH: true, // 搜尋
 
-  NEXT_WIDGET_TO_TOP: true, // 是否显示回顶
-  NEXT_WIDGET_TO_BOTTOM: false, // 显示回底
-  NEXT_WIDGET_DARK_MODE: false, // 显示日间/夜间模式切换
+  NEXT_WIDGET_TO_TOP: true, // GO TOP
+  NEXT_WIDGET_TO_BOTTOM: false, // GO BOTTOM
+  NEXT_WIDGET_DARK_MODE: false, // 日间/夜间模式切换
   NEXT_WIDGET_TOC: true, // 移动端显示悬浮目录
 
   NEXT_ARTICLE_RELATE_POSTS: true, // 相关文章推荐
